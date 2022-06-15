f2cc608c80c73e79a620a414c1cd911c80fe247d
Alisson Hanael Zini Gaio <90325162+AlissonHanael@users.noreply.github.com>
2022-06-15 00:10:32 -0300

    versão 1.0.2
    adicionado sistema de pontuação (incompleto)
    botao de reinicio

--- MARIO_RUN/assets/js/main.js
+++ MARIO_RUN/assets/js/main.js
@@ -1,6 +1,9 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
-
+const restart = document.querySelector('.restart')
+let count = 0
+let record = 0
+let vivo = true
 const jump = () => {
   mario.classList.add('jump')
 
@@ -9,22 +12,60 @@ const jump = () => {
   }, 500)
 }
 
-const loop = setInterval(() => {
-  const pipePosition = pipe.offsetLeft
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+iniciar()
 
-  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.animation = 'none'
-    pipe.style.left = `${pipePosition}px`
-    mario.style.animation = 'none'
-    mario.style.bottom = `${marioPosition}px`
+function iniciar() {
+  pontuacao()
+  setInterval(() => {
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window
+      .getComputedStyle(mario)
+      .bottom.replace('px', '')
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+      vivo = false
+      pipe.style.animation = 'none'
+      pipe.style.left = `${pipePosition}px`
+      mario.style.animation = 'none'
+      mario.style.bottom = `${marioPosition}px`
 
-    mario.src = 'assets/img/game-over.png'
-    mario.style.width = '75px'
-    mario.style.marginLeft = '50px'
+      mario.src = 'assets/img/game-over.png'
+      mario.style.width = '75px'
+      mario.style.marginLeft = '50px'
+      if (record <= count) {
+        record = count
+      }
+      restart.classList.add('replay')
+    }
+    document.getElementById('pontos').innerHTML = count
+    document.getElementById('recorde').innerHTML = record
+  }, 10)
+}
 
-    clearInterval(loop)
+function reiniciar() {
+  restart.classList.remove('replay')
+
+  const pipePosition = -100
+  const marioPosition = 0
+
+  mario.style.bottom = `${marioPosition}px`
+  mario.src = 'assets/img/mario.gif'
+  mario.style.width = '150px'
+  mario.style.marginLeft = '0px' //fazer variaveis global
+  count = 0
+  if (pipePosition <= 120 && marioPosition < 80) {
+    pipe.style.left = ''
+    pipe.style.animation = ''
+    mario.style.animation = ''
+    iniciar()
   }
-}, 10)
+  document.getElementById('pontos').innerHTML = count
+  document.getElementById('recorde').innerHTML = record
+}
+
+function pontuacao() {
+  while (vivo) {
+    count++
+  }
+}
 
 document.addEventListener('keydown', jump)
